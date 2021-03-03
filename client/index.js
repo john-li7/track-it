0ddc21f1c09c9f01cf5b9384b43e3a54e491ccad
johnli <jli159@binghamton.edu>
2021-03-03 11:19:56 -0500

    basic client and server files created

---
+++ client/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<p>Hello World</p>, document.getElementById('root'));
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+app.listen(3000);
+console.log('Node server listening on port 3000');
